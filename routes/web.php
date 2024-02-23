98f4f6e5d0eb34a942c53056c68a2e8856246747
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-02-23 13:16:34 +0100

    cuarto intento

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/health', function () {
+    return response(null, 200);
 });
